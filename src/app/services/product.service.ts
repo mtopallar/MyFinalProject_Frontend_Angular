6f41522399198e3617800e4acb23ae96845cec9c
Murat Topallar <topallarm@outlook.com>
2021-03-21 00:09:50 +0300

    Product Add formu ve metodları yazıldı. Validation hataları handle edildi.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'; //backend e bağlanmak için
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -24,5 +25,9 @@ export class ProductService {
     let newPath =
       this.apiUrl + 'products/getbycategory/?categoryId=' + categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add", product)
   }
 }
